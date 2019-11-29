acf439ea429b32cd8f3e32d1ed3aa74d3750eb91
Aniruddha Thorat <aniruddhathorat123@gmail.com>
2019-11-29 08:38:00 +0530

    classes with getter and setters

--- Aquarium.kt
+++ Aquarium.kt
@@ -11,3 +11,22 @@ class Aquarium {
         get() = length * width * height / 1000
         set(value) { height = (value/1000) / width * length}
 }
+
+fun main() {
+    val food: SimpleSpice = SimpleSpice()
+    println("Name : ${food.name}")
+    println("Spiciness: ${food.heat}")
+}
+class SimpleSpice {
+    var name : String = "curry"
+    var spiciness = "mild"
+    //suspecious var property, it's setter does not influence it's getter result.
+    var heat : Int = 0
+        get() {
+            return when (spiciness) {
+                "mild" -> 5
+                "hot" -> 10
+                else-> 0
+            }
+        }
+}
